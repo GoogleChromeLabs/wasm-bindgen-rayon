3d09066381e8aacd2d95aeffe1e64a92dcd56753
Ingvar Stepanyan <rreverser@google.com>
2021-03-30 17:01:42 +0000

    Fixup command

--- demo/build.sh
+++
@@ -1,4 +0,0 @@
-wasm-pack build --target web
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory' rustup run nightly-2021-02-11 wasm-pack build --target web --out-dir pkg-parallel -- --features parallel -Z build-std=panic_abort,std
-npx webpack build ./index.js --mode production -o dist --output-filename index.js
-cp index.html dist/
